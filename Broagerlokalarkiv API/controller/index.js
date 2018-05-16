494dc905b669a5c39bc7f5266f6a88d0d5dcec61
zantex94 <33219315+zantex94@users.noreply.github.com>
2018-05-16 10:28:40 +0200

    added index to controller

--- Broagerlokalarkiv API/controller/index.js
+++ Broagerlokalarkiv API/controller/index.js
@@ -3,10 +3,17 @@ const game = require('./game');
 const gameQuestion = require('./gamequestion');
 const question = require('./question');
 const roundQuestion = require('./roundquestion');
+
 const router = require('express').Router();
+const bodyParser = require('body-parser').json;
 
 
 router.use('/answer', answer)
+router.use('/game', game);
+router.use('/gamequestion', gameQuestion);
+router.use('/question', question);
+router.use('/roundquestion', roundQuestion);
+router.use(bodyParser);
 
 
 
