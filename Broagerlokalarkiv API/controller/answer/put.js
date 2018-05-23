a3c1da6f53d39133a0ddb845f1b47d15398ca9a2
zantex94 <33219315+zantex94@users.noreply.github.com>
2018-05-23 08:20:36 +0200

    revrt changes

--- Broagerlokalarkiv API/controller/answer/put.js
+++ Broagerlokalarkiv API/controller/answer/put.js
@@ -1,10 +1,8 @@
 ﻿var Answer = require('../../models/answer/answer');
 
 module.exports = (req, res) => {
-    var answer = req.body;
-    Answer.findOneAndUpdate({ answerId: req.params.answerId }, {$set: answer}, function (err, answer) {
+    Answer.findOneAndUpdate({ }, {_id:0, __v:0}, function (err, answer) {
         if (err)
             res.send(err);
-        res.status(205).json({ answer });
     });
 };
